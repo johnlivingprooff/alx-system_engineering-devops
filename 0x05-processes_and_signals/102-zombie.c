37696764d27848343c5c4e9ff6cbf9666978d248
johnlivingprooff <johnlivingprooff@gmail.com>
2023-11-27 14:48:22 +0200

    Fixed the betty bugs (codestyle bugs)

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -10,40 +10,39 @@
 */
 int infinite_while(void)
 {
-    while (1)
-        sleep(1);
-    return (0);
+	while (1)
+		sleep(1);
+	return (0);
 }
 
 /**
  * main - creates 5 zombie processes
- * @n: number of processes created
  * Return: Exit Success!
 */
 int main(void)
 {
-    pid_t child;
-    int id = 5, i;
+	pid_t child;
+	int id = 5, i;
 
-    for (i = 0; i < id; i++)
-    {
-        child = fork();
+	for (i = 0; i < id; i++)
+	{
+		child = fork();
 
-        if (child >= 0)
-        {
-            if (child == 0)
-            {
-                printf("Zombie process created, PID: %d\n", getpid());
-                sleep(2); /*prints within a 1 sec interval*/
-                exit(0);
-            }
-        }
+		if (child >= 0)
+		{
+			if (child == 0)
+			{
+				printf("Zombie process created, PID: %d\n", getpid());
+				sleep(2); /*prints within a 1 sec interval*/
+				exit(0);
+			}
+		}
 
-        else
-            exit(EXIT_FAILURE);
-    }
+		else
+			exit(EXIT_FAILURE);
+	}
 
-    infinite_while();
+	infinite_while();
 
-    return(0);
+	return (0);
 }
