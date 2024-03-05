575b3acb208ccb2de9bfb9e3de7ffc7e38e5073f
johnlivingprooff <johnlivingprooff@gmail.com>
2024-03-05 21:23:54 +0200

    1. Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Prints titles of the first 10 hot posts listed for a given subreddit."""
+import requests
+
+
+def top_ten(subreddit):
+    """Prints titles of the first 10 hot posts listed for a given subreddit."""
+    uri = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {
+        'User-Agent': 'myBrowser/20.15.4548 (Ubuntu 18.04)'
+    }
+    params = {
+        'limit': 10
+    }
+
+    try:
+        resp = requests.get(uri, headers=headers, params=params,
+                            allow_redirects=False)
+        data = resp.json()
+
+        if 'data' in data and 'children' in data['data']:
+            for child in data['data']['children']:
+                print(child['data']['title'])
+        else:
+            print(None)
+    except Exception:
+        print(None)
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,3 @@
+# 0x16. API advanced
+
+This projects to help me understand how to use APIs, and I'll be using python as the code language for this project.
