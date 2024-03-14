c797f8dacc268bd68472f1ca1cf12fdb43b37e08
johnlivingprooff <johnlivingprooff@gmail.com>
2024-03-14 21:31:01 +0200

    0x1B fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,7 +3,6 @@
 exec { 'ulimit_replace':
   provider => 'shell',
   command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-  unless   => 'sudo grep "ULIMIT=\"-n 4096\"" /etc/default/nginx',
   before   => Exec['ulimit_reload'],
 }
 
